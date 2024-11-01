b075e96b7f27c9de5304016d4f49f046f96c1225
PrinceRaval <divya.raval92@gmail.com>
2024-11-01 14:12:49 -0500

    new changes

---
+++ routes/user.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+router.get("/users", () => console.log("GET users"));
+router.get("/users", () => console.log("GET users"));
+router.get("/users", () => console.log("GET users"));
+
+module.exports = router;
