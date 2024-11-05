024490aece864186adf46a37ccba2bc63af8d344
PrinceRaval <divya.raval92@gmail.com>
2024-11-05 08:37:30 -0500

    new changes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router(); // Initialize the router
 const { NOT_FOUND_STATUS_CODE } = require("../utils/errors");
 
 const userRouter = require("./users");
-//const itemRouter = require("./clothingItems");
+const itemRouter = require("./clothingItems");
 
 router.use("/users", userRouter);
-//router.use("/items", itemRouter);
+router.use("/items", itemRouter);
 
 router.use((req, res) => {
   res.status(NOT_FOUND_STATUS_CODE).send({ message: "Router not found" });
