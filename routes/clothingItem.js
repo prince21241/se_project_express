0e67bbfcccce50fe5d47cf0944b799c5216f6313
PrinceRaval <divya.raval92@gmail.com>
2024-11-12 09:10:56 -0500

    new changes

---
+++ routes/clothingItem.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const {
+  createItem,
+  getItems,
+  deleteItem,
+  likeItem,
+  dislikeItem,
+} = require("../controllers/clothingItem");
+
+router.post("/", createItem);
+router.get("/", getItems);
+router.delete("/:itemId", deleteItem);
+router.put("/:itemId/likes", likeItem);
+router.delete("/:itemId/likes", dislikeItem);
+
+module.exports = router;
