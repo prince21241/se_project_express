09af8c43fa55351d0ec194c764654cab0d7282db
PrinceRaval <divya.raval92@gmail.com>
2024-11-12 09:26:59 -0500

    fixed the errors:

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ const router = require("express").Router();
 const { NOT_FOUND_STATUS_CODE } = require("../utils/errors");
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
-router.use("/users", userRouter);
 
+// Add an empty line here to satisfy ESLint's `newline-after-import` rule
+
+router.use("/users", userRouter);
 router.use("/items", itemRouter);
+
 router.use((req, res) => {
   res.status(NOT_FOUND_STATUS_CODE).send({ message: "Router not found" });
 });
+
 module.exports = router;
