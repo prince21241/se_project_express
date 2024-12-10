bf857805765884c47f892b6797a8c2fe9f1df94e
PrinceRaval <divya.raval92@gmail.com>
2024-12-10 08:25:30 -0500

    new changes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,1 +1,26 @@
 const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
+const { UNAUTHORIZED } = require("../utils/errors");
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.status(UNAUTHORIZED).send({ message: "AUTHORIZATION REQUIRED" });
+  }
+
+  const token = authorization.replace("Bearer ", "");
+
+  let payload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return res.status(UNAUTHORIZED).send({ message: "INVALID TOKEN" });
+  }
+
+  req.user = payload;
+
+  return next();
+};
+
+module.exports = auth;
