9a30afc2d84de36040d05f46c412c442544e74fd
PrinceRaval <divya.raval92@gmail.com>
2024-12-13 08:11:29 -0500

    new changes

--- controllers/user.js
+++ controllers/user.js
@@ -7,11 +7,53 @@ const {
   SERVER_ERROR_STATUS_CODE,
 } = require("../utils/errors");
 
-const getUsers = (req, res) => {
+/*const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
     .catch((err) => {
       console.error(err);
+      return res
+        .status(SERVER_ERROR_STATUS_CODE)
+        .send({ message: "An error has occurred on the server" });
+    });
+}; */
+const createUser = (req, res) => {
+  const { email, password, name, avatar } = req.body;
+  if (!email || !password || !name || !avatar) {
+    return res
+      .status(BAD_REQUEST_STATUS_CODE)
+      .send({ message: "All data required" });
+  }
+  return User.findOne({ email })
+    .then((existingUser) => {
+      if (existingUser) {
+        return res
+          .status(CONFLICT_STATUS_CODE)
+          .send({ message: "This email already exists" });
+      }
+      return bcrypt
+        .hash(password, 10)
+        .then((hash) =>
+          User.create({
+            email,
+            password: hash,
+            name,
+            avatar,
+          })
+        )
+        .then((user) => {
+          res
+            .status(201)
+            .send({ email: user.email, name: user.name, avatar: user.avatar });
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res
+          .status(BAD_REQUEST_STATUS_CODE)
+          .send({ message: "Invalid data" });
+      }
       return res
         .status(SERVER_ERROR_STATUS_CODE)
         .send({ message: "An error has occurred on the server" });
