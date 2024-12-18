5f9347099994f6b12804356c15588cfda4210d2f
PrinceRaval <divya.raval92@gmail.com>
2024-12-18 14:50:49 -0500

    new changes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
+const { login } = require("./controllers/user");
+const { createUser } = require("./controllers/user");
 
 const app = express();
 const { PORT = 3000 } = process.env;
