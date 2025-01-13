0ff41d6d3ee9aeec70b8470e02c67496b7dcb47b
PrinceRaval <divya.raval92@gmail.com>
2025-01-13 08:39:22 -0500

    changed the server port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { login } = require("./controllers/user");
 const { createUser } = require("./controllers/user");
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
