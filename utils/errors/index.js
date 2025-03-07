e3a46d7e234c9131cf838e0ee171de7915cdf443
PrinceRaval <divya.raval92@gmail.com>
2025-03-07 13:24:48 -0500

    fixed the import

---
+++ utils/errors/index.js
@@ -0,0 +1,15 @@
+const NotFoundError = require("./NotFoundError");
+const UnauthorizedError = require("./UnauthorizedError");
+const BadRequestError = require("./BadRequestError");
+const ConflictError = require("./ConflictError");
+const ForbiddenError = require("./ForbiddenError");
+const InternalServerError = require("./InternalServerError"); // Fixed typo
+
+module.exports = {
+  BadRequestError,
+  UnauthorizedError,
+  ConflictError,
+  ForbiddenError,
+  InternalServerError,
+  NotFoundError,
+};
